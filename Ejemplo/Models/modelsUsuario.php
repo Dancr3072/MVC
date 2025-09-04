588c2996ff1b218a561817145efd25c2e5f7dc84
daniel celis <dancr.3072@gmail.com>
2025-09-03 21:07:07 -0500

    Actualizacion Ejemplo MVC 03-09-2025

---
+++ Ejemplo/Models/modelsUsuario.php
@@ -0,0 +1,42 @@
+<?php
+
+class Usuario{
+    public $nombre;
+    public $apellidos;
+    public $email;
+    public $password;
+
+
+    function getnombre(){
+        return $this->nombre;
+    }
+
+     function getapellidos(){
+        return $this->apellidos;
+    }
+
+     function getemail(){
+        return $this->email;
+    }
+
+     function getpassword(){
+        return $this->password;
+    }
+
+    function setnombre(){
+        $this->nombre = $nombre;
+    }
+
+    function setapellido(){
+        $this->apellido = $apellido;
+    }
+
+    function setemail(){
+        $this->email = $email;
+    }
+
+    function setpassword(){
+        $this->password = $password;
+    }
+}
+
